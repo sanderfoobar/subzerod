ae6db9a71a1b9271872adbe8d643acb2179e8c14
Sander <sander@sanderf.nl>
2021-10-23 16:35:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
+INSTALL_REQUIRES = [
+    "aiohttp==3.*"
+]
+
 setup(
     name='subzerod',
     version='1.0',
+    author='Sander',
+    author_email='sander@sanderf.nl',
     python_requires='>=3.6',
-    install_requires=['aiohttp'],
-    packages=find_packages()+['.'],
+    packages=['subzerod'],
     include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=INSTALL_REQUIRES,
     entry_points='''
        [console_scripts]
        subzerod=subzerod.cli:cli_run
@@ -14,12 +25,13 @@ setup(
     url='https://github.com/sanderfoobar/subzerod',
     license='WTFPL',
     description='Subdomain enumeration tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
         'Intended Audience :: Telecommunications Industry',
-        "License :: OSI Approved :: WTFPL-Clause",
         'Operating System :: POSIX :: Linux',
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
